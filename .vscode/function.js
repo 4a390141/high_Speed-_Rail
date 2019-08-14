0e0595aaf6f520b3d7dec27eb18a5d0314e540bb
4a390141 <4a390141@stust.edu.tw>
2019-08-14 13:27:43 +0800

    add monkey attack

--- .vscode/function.js
+++
@@ -1,7 +0,0 @@
-function ooxxGetStyle(element,style){
-    if(element){
-        return getComputedStyle(element,null).getPropertyValue(style.toString())
-    }else{
-        return 'no element'
-    }
-}
--- function.js
+++ function.js
@@ -3,3 +3,16 @@ function ooxxGetStyle(element,style){
         return getComputedStyle(element,null).getPropertyValue(style.toString())
     }
 }
+function ooxxMonkeyAttack(param) {
+    if(param){
+        script = document.createElement('script')
+        script.src ='https://cdnjs.cloudflare.com/ajax/libs/gremlins.js/0.1.0/gremlins.min.js'
+        document.getElementsByTagName('head')[0].appendChild(script)
+        setTimeout(() => {
+        gremlins
+            .createHorde()
+            .gremlin(gremlins.species.clicker().clickTypes(['click']))
+            .unleash({ nb: 1000 })
+        }, 3000)
+    }
+}
