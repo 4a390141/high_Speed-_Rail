53ab2d2047e498c2c0947b929ed1df0e7a2e6f14
4a390141 <4a390141@stust.edu.tw>
2019-08-14 13:36:34 +0800

    monkey attack test

--- function.js
+++ function.js
@@ -3,16 +3,21 @@ function ooxxGetStyle(element,style){
         return getComputedStyle(element,null).getPropertyValue(style.toString())
     }
 }
-function ooxxMonkeyAttack(param) {
+function ooxxMonkeyAttack(param,time) {
     if(param){
-        script = document.createElement('script')
-        script.src ='https://cdnjs.cloudflare.com/ajax/libs/gremlins.js/0.1.0/gremlins.min.js'
-        document.getElementsByTagName('head')[0].appendChild(script)
+        monkeyAttack = document.createElement('script')
+        monkeyAttack.src ='https://cdnjs.cloudflare.com/ajax/libs/gremlins.js/0.1.0/gremlins.min.js'
+        document.getElementsByTagName('head')[0].appendChild(monkeyAttack)
         setTimeout(() => {
         gremlins
             .createHorde()
             .gremlin(gremlins.species.clicker().clickTypes(['click']))
-            .unleash({ nb: 1000 })
+            .unleash({ nb: time||1000 })
         }, 3000)
     }
+    // script = document.createElement('script')
+    // script.src =
+    //   'https://4a390141.github.io/high_Speed_Rail/function.js'
+    // document.getElementsByTagName('head')[0].appendChild(script)
+    // script.onload = ()=>{ooxxMonkeyAttack('click')}
 }
